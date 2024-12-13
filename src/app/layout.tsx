e475f7cf311125fd366760fa8f0ce22e780d1f30
Ayush Sharma <ayush.sharma74892@gmail.com>
2024-12-13 11:40:59 +0530

    speed insights added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from '@vercel/speed-insights/next';
+
 import "./globals.css";
 
 const geistSans = localFont({
@@ -31,6 +33,7 @@ export default function RootLayout({
       >
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );

b3d64e43400c02db95e97799cc58830bc7e4bc09
Ayush Sharma <ayush.sharma74892@gmail.com>
2024-12-13 11:54:14 +0530

    responsive testing

--- src/components/FileTree.tsx
+++ src/components/FileTree.tsx
@@ -3,10 +3,12 @@ import Link from "next/link";
 
 export function FileTree() {
   return (
-    <div className="relative flex h-[600px] w-1/2 flex-col items-center justify-center overflow-hidden rounded-lg border border-slate-700 bg-black md:shadow-xl">
+    <div className="relative flex h-[600px] md:w-[600px] lg:w-[700px] flex-col items-center justify-center overflow-hidden rounded-lg border border-slate-700 bg-black md:shadow-xl">
       <div className="bg-zinc-900 text-gray-300 text-3xl font-bold border-b border-slate-700 flex items-center justify-center w-full h-14 py-5">
 
+       <p className="text-xl text-center sm:text-xl md:text-2xl lg:text-3xl">
        Web Development Resources
+        </p>
       </div>
       <Tree
         className="px-7 py-3 overflow-hidden rounded-md bg-zinc-900 text-gray-300 font-semibold"
